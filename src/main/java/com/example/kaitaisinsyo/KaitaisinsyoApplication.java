5762efe2f97a2ccf622309c49eb8421b73e142c6
yuki <aoiro.savant@gmail.com>
2021-11-01 06:31:00 +0900

    Gitテスト

--- src/main/java/com/example/kaitaisinsyo/KaitaisinsyoApplication.java
+++ src/main/java/com/example/kaitaisinsyo/KaitaisinsyoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KaitaisinsyoApplication {
 
     public static void main(String[] args) {
+        System.out.println("開始");
         SpringApplication.run(KaitaisinsyoApplication.class, args);
     }
 
